fbf88b140626d4ad3b6e71fcec92845928675a8b
ReMinoer <rmorillon@freneticexperience.com>
2014-09-14 13:58:41 +0200

    Fix test paths

--- CoverMyOST.Test/TestPaths.cs
+++ CoverMyOST.Test/TestPaths.cs
@@ -6,12 +6,12 @@ namespace CoverMyOST.Test
     {
 		static public readonly string RootDirectory = Path.GetFullPath("Content/");
 
-		static public readonly string MusicDirectory = Path.Combine(RootDirectory, "Music/");
-        static public readonly string MusicA = Path.Combine(MusicDirectory, "musicA.mp3");
-        static public readonly string MusicB = Path.Combine(MusicDirectory, "musicB.mp3");
-		static public readonly string MusicC = Path.Combine(MusicDirectory, "dir/musicC.mp3");
+		static public readonly string MusicDirectory = Path.GetFullPath(Path.Combine(RootDirectory, "Music/"));
+        static public readonly string MusicA = Path.GetFullPath(Path.Combine(MusicDirectory, "musicA.mp3"));
+        static public readonly string MusicB = Path.GetFullPath(Path.Combine(MusicDirectory, "musicB.mp3"));
+		static public readonly string MusicC = Path.GetFullPath(Path.Combine(MusicDirectory, "dir/musicC.mp3"));
 
-		static public readonly string CoverDirectory = Path.Combine(RootDirectory, "Covers/");
-        static public readonly string CoverA = Path.Combine(CoverDirectory, "coverA.jpg");
+		static public readonly string CoverDirectory = Path.GetFullPath(Path.Combine(RootDirectory, "Covers/"));
+        static public readonly string CoverA = Path.GetFullPath(Path.Combine(CoverDirectory, "coverA.jpg"));
     }
 }
