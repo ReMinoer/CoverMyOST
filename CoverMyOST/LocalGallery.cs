6cd1ed14506ebfd9c62468c73a86c49ac8deb537
ReMinoer <rmorillon@freneticexperience.com>
2014-09-15 21:14:15 +0200

    Restriction to image files for LocalGallery

--- CoverMyOST/LocalGallery.cs
+++ CoverMyOST/LocalGallery.cs
@@ -8,6 +8,7 @@ namespace CoverMyOST
     public class LocalGallery : ICoversGallery
     {
         public string RootPath { get; private set; }
+        private readonly string[] _imagePatterns = {"*.jpg", "*.png", "*.gif"};
 
         public LocalGallery(string path)
         {
@@ -16,10 +17,18 @@ namespace CoverMyOST
 
         public Dictionary<string, Bitmap> Search(string query)
         {
-            IEnumerable<string> files = Directory.EnumerateFiles(RootPath);
-            string file = files.First(f => Path.GetFileNameWithoutExtension(f) == query);
+            IEnumerable<string> files =
+                _imagePatterns.AsParallel().
+                               SelectMany(imagePattern => Directory.EnumerateFiles(RootPath, imagePattern)).
+                               Where(file =>
+                               {
+                                   string filename = Path.GetFileNameWithoutExtension(file);
+                                   return filename != null && filename.Contains(query);
+                               });
 
-            var result = new Dictionary<string, Bitmap> {{file, new Bitmap(Image.FromFile(file))}};
+            var result = new Dictionary<string, Bitmap>();
+            foreach (string file in files)
+                result.Add(file, new Bitmap(Image.FromFile(file)));
             return result;
         }
     }
